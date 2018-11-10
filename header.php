89e3d62a19cb169c43dfaff4bc759fb9de3fb180
greg57470 <44603862+greg57470@users.noreply.github.com>
2018-11-10 16:53:02 +0100

    Add files via upload

---
+++ header.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Père Ouvrard</title>
+	<meta charset="utf-8">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script><!-- JQuery -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"> <!-- Bootstrap -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script> <!-- JavaScript for bootstrap -->
+	<link rel="stylesheet" type="text/css" href="css/style.css" /> <!-- ficher css -->
+	<link href="https://fonts.googleapis.com/css?family=Montserrat|Parisienne" rel="stylesheet">  <!-- Montserrat + Great Vibes google font -->
+	<script src="https://maps.googleapis.com/maps/api/js?sensor=false" type="text/javascript"></script>
+	<script src="/assets/gmap3.js?body=1" type="text/javascript"></script><!-- API google map -->
+
+
+</head>
+<body>
+	<nav role="navigation" class="navbar navbar-expand-lg navbar-inverse fixed-top">
+		<div class="container">
+			<div class="navbar-header">
+				<button type="button" data-target="#navbarCollapse" data-toggle="collapse" class="navbar-toggle">
+					<span class="sr-only">Menu</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+			</div>
+			<div id="navbarCollapse" class="collapse navbar-collapse">
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="#Accueil">Accueil</a></li>
+					<li><a href="#Restaurant">Restaurant</a></li>
+					<li><a href="#Privatisation">Privatisation</a></li>
+					<li><a href="#Soirées">Soirées</a></li>
+					<li><a href="#Contact">Contact</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+	<header class="container-fluid">
+		<div class="row">
+			<div class="col-lg-12 col-md-12 col-xs-12" id="background">
+				<h1 class="text-center"><span id="Cap">Cap Ferret</span> <br>LE PERE OUVRARD</h1>
+			</div>
+		</div>
+	</header>
---
+++ index.php
@@ -0,0 +1,78 @@
+<?php include_once 'header.php';?>
+
+<!-- Section Histoire -->
+
+<div class="container-fluid" id="Histoire">
+	<div class="row">
+		<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+			<h2 class="text-center">Histoire</h2>
+		</div>
+		<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+			<p class="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin at consectetur quam, vitae elementum arcu. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin quis lacinia nisl, eu consectetur orci. Fusce in arcu ligula. Proin eros nisl, iaculis non orci eget, hendrerit gravida quam. Aenean faucibus egestas ipsum, sit amet pretium ipsum pulvinar quis. Ut vel nisl eu diam interdum sagittis id pulvinar justo. Duis lacinia, eros eget suscipit ullamcorper, felis quam ultrices nibh, ut posuere felis dui at purus. Maecenas aliquam mollis quam, porta consequat purus vehicula vel. Nullam ac ante fermentum, porta urna ut, varius lectus. Aliquam fermentum vitae nulla a tristique. Aenean sollicitudin fermentum mauris, et lobortis risus laoreet ac. Sed elit diam, hendrerit nec commodo id, bibendum eget felis. </p>
+		</div>
+	</div>	<!-- end row -->
+</div><!-- end container -->
+
+<!-- Section Culinaire -->
+
+<div class="container-fluid"  id="culinaire">
+	<div class="row">
+		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+			<h2 class="text-center">Style Culinaire</h2>
+		</div>
+	</div>	
+</div> <!-- Title + banner -->
+
+<div class="container-fluid" id="plat">
+	<div class="row">
+		<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12" id="plat1">
+			<img src="image/moule.jpg" class="img-rounded img-responsive">
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin at consectetur quam, vitae elementum arcu. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+		</div>
+		<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12" id="plat2">
+			<img src="image/plat1.jpg" class="img-rounded img-responsive">
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin at consectetur quam, vitae elementum arcu. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+		</div>
+		<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+			<img src="image/plat2.jpg" class="img-rounded img-responsive" id="plat3">
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin at consectetur quam, vitae elementum arcu. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+		</div>
+	</div><!-- end row -->
+</div><!-- end container -->
+
+<!-- Section Horaire -->
+
+<div class="container-fluid" id="Horaire">
+	<div class="row">
+		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+			<h2 class="text-center">Horaire</h2>
+
+		</div>
+
+		<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+			<p class="text-center">Lundi - Dimanche</p>
+		</div>
+
+		<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+			<p class="text-center">12h - 14h / 19h - 22h</p>
+		</div>
+
+	</div><!-- end row -->
+</div> <!-- end container -->
+
+<!-- Section google map -->
+
+ <div class="container-fluid" id="map">
+	<div class="row">
+		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+			<h2 class="text-center">ADRESSE</h2>
+		</div>
+		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2838.6880687595017!2d-1.2489586840605689!3d44.644291979099826!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd5361a68869aa2f%3A0x31867705a71f7202!2sLe+P%C3%A8re+Ouvrard!5e0!3m2!1sfr!2sfr!4v1541780989094" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+		</div>
+	</div>	
+</div> 
+
+
+
+
