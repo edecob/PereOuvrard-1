f85d339307fedcce6c3282b5bc1fcb4a09d5e80c
edecob <44600632+edecob@users.noreply.github.com>
2018-11-21 08:36:48 +0100

    Creater footer de Soumaya
    Style css à mettre dans un fichier à part
    J'ai déjà corrigé le logo qui n'était pas adapté au web

---
+++ footer.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Père Ouvrard</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width,initial scale=1"><!-- zoom initiale le zoom s'adaptera au site  La largeur de mon site correspondra à la largeur de l'appareil-->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script><!-- JQuery -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"> <!-- Bootstrap -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script> <!-- JavaScript for bootstrap -->
+        <link href="https://fonts.googleapis.com/css?family=Montserrat|Parisienne" rel="stylesheet">  <!-- Montserrat + Great Vibes google font -->
+        <!-- font Awesome -->
+
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/solid.css" integrity="sha384-wnAC7ln+XN0UKdcPvJvtqIH3jOjs9pnKnq9qX68ImXvOGz2JuFoEiCjT8jyZQX2z" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/regular.css" integrity="sha384-zkhEzh7td0PG30vxQk1D9liRKeizzot4eqkJ8gB3/I+mZ1rjgQk+BSt2F6rT2c+I" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/brands.css" integrity="sha384-nT8r1Kzllf71iZl81CdFzObMsaLOhqBU1JD2+XoAALbdtWaXDOlWOZTR4v1ktjPE" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/fontawesome.css" integrity="sha384-HbmWTHay9psM8qyzEKPc8odH4DsOuzdejtnr+OFtDmOcIVnhgReQ4GZBH7uwcjf6" crossorigin="anonymous">
+
+        <link rel="stylesheet" type="text/css" href="css/style.css" /> <!-- ficher css -->
+       
+    </head>
+
+
+    <body>
+        <footer class="container-fluid footer">
+		
+			
+					<!-- Logo-Popup -->
+					
+										<div class="row">
+												<div class="col-md-4 col-lg-4 col-xs-12 col-sm-12">
+													<img src="image/logo-pere-ouvrard-footer.png" class="img-responsive"  onmouseup="document.getElementById('popup').style.display = 'block';" style="padding-top: 1%;
+    width: 40%;margin:0 auto;">
+													
+														
+													<div class="hover_bkgr_fricc" style="width:100%"style="display: none" id="popup">
+															<span class="helper"></span>
+															<div class="popcolor">
+																<div class="popupCloseButton" onmouseup="document.getElementById('popup').style.display = 'none';">X</div>
+																<p><strong>LE PERE OUVRARD</strong></p>
+																<p>4 rue des Pionniers,</p>
+																<p>33950 Cap-Ferret</p> </br></br>
+
+																<p><strong>HEURES D'OUVERTURES :</strong></p> </br>
+
+																<p>Lundi 9  : 00 - 22 : 00</p> 
+																<p>Jeudi-Dimanche : 12 : 00 - 14 : 00</p> 
+																<p>20 : 00- 22 :00</p> </br>
+																<p>Tél : +33 5 57 18  75 61</p>
+															</div>
+													</div>
+												</div>
+					<!-- Nametag -->
+												<div class="col-md-4 col-lg-4 col-xs-12 col-sm-12">
+													<a href=" https://www.instagram.com/lepereouvrard/?hl=fr "><img class="img-responsive" style="width:30%;margin:0 auto;margin-top: 5%" src="image/nametag-pere-ouvrard.png" id="nametag" alt="nametag instagram"></a> 
+												</div>
+
+												<div class="col-md-4 col-lg-4 col-xs-12 col-sm-12">
+													<div class="contact" style="text-align:left;margin-left:-7%;">
+														<ul class="adresse">
+															<li>4 rue des pionniers au Cap-Ferret</li>
+															<li>33970 Lège-Cap-Ferret </li>
+															<li>05 57 18 75 61</li>																											
+														</ul>
+														<ul class="icons">
+															<li><span class="dot"><a href="https://www.facebook.com/LePereOuvrard"> <i class="fab fa-facebook-f fa-2x"></i></a></span></li>
+															<li><span class="dot"><a href=" https://www.instagram.com/lepereouvrard/?hl=frhttps://www.instagram.com/lepereouvrard/?hl=fr "> <i class="fab fa-instagram fa-2x"></i></a></span></li>
+															<li><span class="dot"><a href="https://www.tripadvisor.fr/Restaurant_Review-g776256-d13961868-Reviews-Le_Pere_Ouvrard-Lege_Cap_Ferret_Gironde_Nouvelle_Aquitaine.html"><i class="fab fa-tripadvisor fa-2x"></i></a></span></li>
+
+														</ul>
+													</div>
+												</div>
+
+										</div>
+
+
+					<!-- Copyright -->
+										<div class="row text-center">
+												<div class="col-md-12 col-lg-12 col-xs-12 col-sm-12">
+													
+														<span  id="copyright">&copy Copyright 2018 Agence Gremaya / <a href="#" id="mentions"> Mentions légales</a> </span>
+					
+													
+												</div >
+										</div>
+										
+										
+	
+										
+										
+										
+								
+	
+						
+					
+        </footer>
+
+    </body>
+</html>
